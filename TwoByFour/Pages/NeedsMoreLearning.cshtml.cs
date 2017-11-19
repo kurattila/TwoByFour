585c97f54a35c5248cfa0a7525a1946b4ca3590c
Attila Kur <attila.kur@kistler.com>
2017-11-19 12:25:36 +0100

    Page of "WhatNeedsMoreLearning" added

---
+++ TwoByFour/Pages/NeedsMoreLearning.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using TwoByFour.MultiplicationTable;
+
+namespace TwoByFour.Pages
+{
+    public class NeedsMoreLearningModel : PageModel
+    {
+        private readonly ITrainingCourse _course;
+
+        public NeedsMoreLearningModel(ITrainingCourse course)
+        {
+            _course = course;
+        }
+
+        public Multiplication[] Items => _course.NeedMoreLearningChallenges;
+    }
+}
